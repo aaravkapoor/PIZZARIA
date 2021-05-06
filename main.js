d7c2153725848731009f68fcebef224ad8b1b937
aaravkapoor <76781948+aaravkapoor@users.noreply.github.com>
2021-05-06 13:24:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function add_item()
     htmldata="<section class='cards'>";
     for(var i=0; i<menu_list_array.length;i++)
     {
-        htmldata += '<div class="card">' +'<img src="images/pizzaImg.png"/>' +menu_list_array[i] + '</div>';
+        htmldata += '<div class="card">' +'<img src="pizzaImg.png"/>' +menu_list_array[i] + '</div>';
     }  
     htmldata=htmldata+ "</section>"
     document.getElementById("display_addedmenu").innerHTML = htmldata;
